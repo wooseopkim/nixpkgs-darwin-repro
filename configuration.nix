070b4b9a933ebad76816481d0b68a193403ca4d2
Wooseop Kim <wooseop@protonmail.com>
2025-02-04 02:37:52 +0900

    Wrap function call in parentheses

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,7 @@
 # Stolen from https://github.com/LnL7/nix-darwin/blob/49b807fa7c37568d7fbe2aeaafb9255c185412f9/modules/examples/simple.nix
 { pkgs, ... }:
 {
-  nix.nixPath = [ builtins.getEnv "NIX_PATH" ];
+  nix.nixPath = [ (builtins.getEnv "NIX_PATH") ];
 
   environment.systemPackages = [ pkgs.vim ];
 
