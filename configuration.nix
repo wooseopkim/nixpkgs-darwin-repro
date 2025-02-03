fa065ca02a484ed7f7f7c92d2e2f3d1f1b6131e6
Wooseop Kim <wooseop@protonmail.com>
2025-02-04 02:21:24 +0900

    Remove unused parameter

--- configuration.nix
+++ configuration.nix
@@ -1,5 +1,5 @@
 # Stolen from https://github.com/LnL7/nix-darwin/blob/49b807fa7c37568d7fbe2aeaafb9255c185412f9/modules/examples/simple.nix
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 {
   environment.systemPackages = [ pkgs.vim ];
 
