cce1a414c3a33f52cac93354e5d2d06d5f00d29c
Wooseop Kim <wooseop@protonmail.com>
2025-02-04 02:34:27 +0900

    Override `nix.nixPath` manually
    <https://github.com/LnL7/nix-darwin/issues/137#issuecomment-488049683>

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,8 @@
 # Stolen from https://github.com/LnL7/nix-darwin/blob/49b807fa7c37568d7fbe2aeaafb9255c185412f9/modules/examples/simple.nix
 { pkgs, ... }:
 {
+  nix.nixPath = [ builtins.getEnv "NIX_PATH" ];
+
   environment.systemPackages = [ pkgs.vim ];
 
   system.stateVersion = 6;
