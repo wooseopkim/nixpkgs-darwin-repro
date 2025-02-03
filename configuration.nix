c99e180119915c401b7b961be477ce77b243cb52
Wooseop Kim <wooseop@protonmail.com>
2025-02-04 02:29:13 +0900

    Revert "Try removing the comment"
    This reverts commit 7d2b2a6ca68e9fda7cce9dbf95fda39bb613ff8d.

--- configuration.nix
+++ configuration.nix
@@ -1,3 +1,4 @@
+# Stolen from https://github.com/LnL7/nix-darwin/blob/49b807fa7c37568d7fbe2aeaafb9255c185412f9/modules/examples/simple.nix
 { pkgs, ... }:
 {
   environment.systemPackages = [ pkgs.vim ];
